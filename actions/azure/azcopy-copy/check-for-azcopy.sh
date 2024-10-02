9f6f556c3802c3f6ae31fbbebb8c00775af36a7b
Alex Axthelm <aaxthelm@RMI.org>
2024-10-02 13:15:53 +0200

    Run installation if not installed

--- actions/azure/azcopy-copy/check-for-azcopy.sh
+++ actions/azure/azcopy-copy/check-for-azcopy.sh
@@ -1,12 +1,14 @@
 #! /bin/sh
 azcopy_path="$(command -v azcopy 2>&1 >/dev/null)"
 if [ -n "$azcopy_path" ]
-echo "azcopy is installed at: $azcopy_path"
 then
   echo "azcopy is installed at: $azcopy_path"
+  echo "azcopy-installed=true"
   echo "azcopy-installed=true" >> "$GITHUB_OUTPUT"
 else
   echo "azcopy is not installed."
+  echo "azcopy-installed=false"
   echo "azcopy-installed=false" >> "$GITHUB_OUTPUT"
 fi
 
+echo "azcopy is installed at: $azcopy_path"
