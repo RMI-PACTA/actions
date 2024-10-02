fe1ae5e76343e25999d06354a9694b0476efbeff
Alex Axthelm <aaxthelm@RMI.org>
2024-10-02 11:35:39 +0200

    Group installation output

--- actions/azure/azcopy-copy/install-azcopy.sh
+++ actions/azure/azcopy-copy/install-azcopy.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
-sudo apt-get update
+echo "::group::Install azcopy"
 sudo apt-get install -y curl
 curl -sSL -O https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 sudo apt-get update
 sudo apt-get install -y azcopy
+echo "::endgroup::"
 
 # Show version
 echo "azcopy version:"
